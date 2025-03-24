c6da17abbb8ff64bcfb50b1b0d5f73ff9e0999d8
Mine Benitez <mine-1301@hotmail.com>
2025-03-24 11:34:07 -0600

    genrando funcion footer y barra de Navegaion

--- editor.php
+++ editor.php
@@ -13,55 +13,7 @@ verificarSesion();
         <div class="row d-flex flex-nowrap" style="min-height: 100vh">
             <div class="col p-0 d-flex flex-column">
                 <!-- Barra Superior -->
-                <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-                    <div class="container-fluid">
-                        <!-- Icono y Nombre -->
-                        <a class="navbar-brand" href="#">
-                            <img src="logoproyecto.webp" alt="Bootstrap" width="30" height="30" />
-                            UDC
-                        </a>
-                        <button
-                            class="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <!-- Opciones del Menú -->
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="inicio.php">Inicio</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Perfil</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Ver Libros</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Categorias</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Editor de Libros</a>
-                                </li>
-                            </ul>
-                            <form class="d-flex" role="search">
-                                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" />
-                                <button class="btn btn-outline-success" type="submit">
-                                    <i class="fa-solid fa-magnifying-glass"></i>
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </nav>
-
+                <?php echo generarBarraNav(); ?>
                 <div class="container-sm flex-grow-1 mt-lg-4">
                     <div class="row">
                         <div class="col-12 gap-3">
@@ -173,26 +125,7 @@ verificarSesion();
                         </div>
                     </div>
                 </div>
-
-                <!-- Footer -->
-                <div class="container-fluid">
-                    <div class="row bg-secondary">
-                        <!-- Columnas Responsivas -->
-                        <!-- Van divididas en 12 partes, la sm(small) sólo va dividida en 4  -->
-                        <div class="col-6">
-                            <h4>"Unidos por el saber, desde cualquier rincón del planeta"</h4>
-                            <h5>Redes sociales</h5>
-                            <i class="fa-brands fa-facebook"></i>
-                            <a class="btn" href="#" role="button">facebook</a>
-                            <h6>Creado por: Minerva Benítez Pérez 2025</h6>
-                        </div>
-                        <div class="col-6 d-flex flex-column">
-                            <a class="btn" href="inicio.php" role="button">Inicio</a>
-                            <a class="btn" href="ayuda.php" role="button">Ayuda</a>
-                            <a class="btn" href="mision.php" role="button">Mision</a>
-                        </div>
-                    </div>
-                </div>
+                <?php echo generarFooter(); ?>
             </div>
         </div>
     </div>
--- inicio.php
+++ inicio.php
@@ -8,80 +8,8 @@ verificarSesion();
 <html lang="es-MX" data-bs-theme="dark">
 <?php echo generarEncabezado('Inicio'); ?>
     <body>
-        <!-- Barra Superior -->
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <!-- Icono y Nombre -->
-                <a class="navbar-brand" href="#">
-                    <img
-                        src="logoproyecto.webp"
-                        alt="Bootstrap"
-                        width="30"
-                        height="30"
-                    />
-                    UDC
-                </a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <!-- Opciones del Menú -->
-                <div
-                    class="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                >
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a
-                                class="nav-link active"
-                                aria-current="page"
-                                href="#"
-                                >Inicio</a
-                            >
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="perfil.php">Perfil</a>
-                        </li>
+    <?php echo generarBarraNav(); ?>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="listado.php">Ver Libros</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="categorias.php">Categorias</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="editor.php">Agregar Libros</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input
-                            class="form-control me-2"
-                            type="search"
-                            placeholder="Buscar"
-                            aria-label="Buscar"
-                        />
-                        <button class="btn btn-outline-success" type="submit">
-                            <i class="fa-solid fa-magnifying-glass"></i>
-                        </button>
-                    </form>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Cerrar Sesión</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Fin Barra Superior -->
         <!-- Contenido -->
         <div class="container-md mt-4">
             <!-- Fila -->
@@ -214,29 +142,6 @@ verificarSesion();
             <br />
         </div>
 
-        <!-- Footer -->
-        <div class="container-fluid">
-            <div class="row bg-secondary">
-                <!-- Columnas Responsivas -->
-                <!-- Van divididas en 12 partes, la sm(small) sólo va dividida en 4  -->
-                <div class="col-6">
-                    <h4>
-                        "Unidos por el saber, desde cualquier rincón del
-                        planeta"
-                    </h4>
-                    <h5>Redes sociales</h5>
-                    <i class="fa-brands fa-facebook"></i>
-                    <a class="btn" href="https://www.facebook.com/" role="button">facebook</a>
-                    <h6>Creado por: Minerva Benítez Pérez 2025</h6>
-                </div>
-                <div class="col-6 d-flex flex-column">
-                <a class="btn" href="inicio.php" role="button">Inicio</a>
-                    <a class="btn" href="ayuda.php" role="button">Ayuda</a>
-                    <a class="btn" href="mision.php" role="button">Mision</a>
-                    
-                    
-                </div>
-            </div>
-        </div>
+        <?php echo generarFooter(); ?>
     </body>
 </html>
