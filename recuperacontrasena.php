80f610720adbb20894235351cb05076c46196c2b
Luis Mario Estrada Pereyra <43025205+PaletaGalleta@users.noreply.github.com>
2025-05-21 14:40:15 -0600

    Implementar funcionalidad de recuperación de contraseña con envío de correo

--- recuperacontrasena.php
+++ recuperacontrasena.php
@@ -1,7 +1,50 @@
 <?php
 session_start();
 require 'utils.php';
+require "config.php";
+
+if (isset($_POST['correo'])) {
+    $correo = $_POST['correo'];
+    $instruccion = "SELECT * FROM usuario where correoElectronico=:x";
+
+    $query = $connection->prepare($instruccion);
+    $query->bindParam("x", $correo, PDO::PARAM_STR);
+    $query->execute();
+    $respuesta = $query->fetch(PDO::FETCH_ASSOC);
+
+    // Verifica si el correo existe en la base de datos
+    $usuario = $respuesta['correoElectronico'];
+
+    if ($usuario) {
+        // Generar Token
+        //TODO
+
+        // Enviar correo
+        $to = $correo;
+        $subject = "Recuperar Contraseña";
+        $message = "Hola, para restablecer tu contraseña, introduce el siguiente token: $token";
+        $headers = "From: Bookia";
+        mail($to, $subject, $message, $headers);
+
+
+        // Guardar token en BDD
+        //TODO
+    } else {
+        // Correo no existe
+    }
+    // Mostrar mensaje de éxito
+    $msj = "Si el correo está registrado, se enviará un enlace para restablecer la contraseña.";
+}
+
+$paso1 = <<<HTML
+    <label for="correo">Escribe tu correo:</label>
+    <input type="email" name="correo" required style="width: 300px;">
+    <div class="mb-3 mt-4">
+        <button class="btn btn-primary " type="submit">Enviar enlace</button>
+    </div>
+    HTML;
 ?>
+
 <!DOCTYPE html>
 <html lang="es-MX" data-bs-theme="dark">
 
@@ -21,15 +64,25 @@ require 'utils.php';
                 <h3>¡Recuperar Contraseña!</h3>
 
 
-                <label for="correo">Escribe tu correo:</label>
-                <input type="email" name="correo" required style="width: 300px;">
-                <div class="mb-3 mt-4">
-                    <button class="btn btn-primary " type="submit">Enviar enlace</button>
-                </div>
+                <?php
+                // Verifica si el formulario fue enviado
+                if (isset($_POST['correo'])) {
+                    // Si el correo existe, muestra el mensaje de éxito
+                    echo $paso2;
+                } else {
+                    // Si el formulario no fue enviado, muestra el formulario
+                    echo $paso1;
+                }
+                ?>
             </div>
         </form>
 
     </div>
+    <?php
+    if (isset($msj)) {
+        echo "<script>alert('$msj');</script>";
+    }
+    ?>
 </body>
 
 </html>
